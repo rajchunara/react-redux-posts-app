69eeeebbecdbcf45a2338be97e8f62d7e0174222
rajchunara <rajchunara777@gmail.com>
2020-05-22 08:53:32 -0400

    Updates

---
+++ precache-manifest.9f9ccdd685a6492920fe2ac864e2d652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e1c61f5ee80e0c1157c5780a7bbe53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1196ab552c5a493a7b10",
+    "url": "/static/css/main.a833f9f7.chunk.css"
+  },
+  {
+    "revision": "94917cf7b6e3848c8bc5",
+    "url": "/static/js/2.4e722790.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4e722790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1196ab552c5a493a7b10",
+    "url": "/static/js/main.6c04a6ad.chunk.js"
+  },
+  {
+    "revision": "67318250c39d774f4cf4",
+    "url": "/static/js/runtime-main.83c2fcc3.js"
+  }
+]);
